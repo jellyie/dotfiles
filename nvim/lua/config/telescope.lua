03bc87dd9ca4984f46a9c773bd13c65b8dece84d
jellyie <okirynn@gmail.com>
2021-12-08 19:25:29 -0500

    update telescope keybindings

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -3,11 +3,21 @@ local actions = require('telescope.actions')
 
 telescope.setup{
   defaults = {
+    initial_mode = 'normal',
     mappings = {
       n = {
         ["q"] = actions.close,
       },
     },
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+    file_browser = {
+      theme = 'ivy',
+      hidden = true,
+    }
   }
 }
 
